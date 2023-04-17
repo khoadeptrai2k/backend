7d9f6de0b1b6b1ee964618f101e21064091c2607
khoadeptrai2k <khoa.huynh.spm@gmail.com>
2023-04-18 00:46:26 +0700

    Nguyet--MANAGER crud STAFF

--- server/routes/manager.route.js
+++ server/routes/manager.route.js
@@ -10,4 +10,10 @@ router.post("/createUserGroup", auth, authManager, ManagerController.createUserG
 router.patch("/updateUserGroup/:id", auth, authManager, ManagerController.updateUserGroup)
 router.delete("/deleteUserGroup/:id", auth, authManager, ManagerController.deleteUserGroup)
 
+router.get("/getStaff", auth, authManager, ManagerController.getListStaff)
+router.post("/createStaff", auth, authManager, ManagerController.createStaff)
+router.patch("/updateStaff/:id", auth, authManager, ManagerController.updateStaff)
+router.delete("/deleteStaff/:id", auth, authManager, ManagerController.deleteStaff)
+
+
 module.exports = router;
